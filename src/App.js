c0a27276ed66d88d2cacc196822c63544fd5df81
Yuvraj Singh Choudhary <yuvi.choudhary03@gmail.com>
2020-06-16 13:53:20 +0530

    Conponent P1

--- src/App.js
+++ src/App.js
@@ -2,19 +2,18 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponent";
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        Navbar dark color = "primary" >
-        <
-        div className = "container" >
-        <
-        NavbarBrand href = "/" > Ristorante Con Fusion < /NavbarBrand>{" "} <
-        /div>{" "} <
-        /Navbar>{" "} <
-        /div>
+    return (
+        <div className="App" >
+            <Navbar dark color="primary" >
+                <div className="container" >
+                    <NavbarBrand href="/"> Ristorante Con Fusion</NavbarBrand>
+                </div>
+            </Navbar>
+            <Menu />
+        </div>
     );
 }
 
