8aa54fb75d340f2b1858f6dc698a6d195d0e736d
Yuvraj Singh Choudhary <yuvi.choudhary03@gmail.com>
2020-06-17 11:10:13 +0530

    Presentational and Container Components

--- src/components/DishdetailComponent .js
+++ src/components/DishdetailComponent .js
@@ -25,7 +25,13 @@ class DishDetail extends Component {
                                 return (
                                     <div key={comment.id}>
                                         <p>{comment.comment}</p>
-                                        <p>-- {comment.author}, {comment.date}</p>
+                                        <p>-- {comment.author}, {new Intl.DateTimeFormat(
+                                            'en-US',
+                                            {
+                                                year: 'numeric',
+                                                month: 'short',
+                                                day: '2-digit'
+                                            }).format(new Date(Date.parse(comment.date)))}</p>
                                     </div>
                                 );
                             })}
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -10,19 +10,14 @@ class Menu extends Component {
         };
     }
 
-    onDishSelect(dish) {
-        this.setState({
-            selectedDish: dish
-        });
-    }
-
+   
 
 
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                    <Card onClick={()=> this.props.onClick(dish.id)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay body className="ml-5">
                             <CardTitle >{dish.name}</CardTitle>
@@ -38,7 +33,7 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                <DishDetail dish={this.state.selectedDish} />
+                
             </div>
         );
     }
